21a67ce303c19667326330266f79638916feb8ff
kevin Cleathero <kengc@hotmail.com>
2017-06-07 17:12:04 -0700

    peoject setup

---
+++ Entering a House/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Entering a House
+//
+//  Created by Kevin Cleathero on 2017-06-07.
+//  Copyright © 2017 Kevin Cleathero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
